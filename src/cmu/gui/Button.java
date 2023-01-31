a19ca51ccc379d97d93eebfaf1593d573707e038
automatopaste <70458229+automatopaste@users.noreply.github.com>
2023-01-31 16:09:23 +0800

    gui update

--- src/cmu/gui/Button.java
+++ src/cmu/gui/Button.java
@@ -77,7 +77,7 @@ public class Button implements Element {
             glTranslatef((int) (params.width * 0.5f), (int) (-params.height * 0.5f), 0f);
             text.render(scale, loc, events);
         } else {
-            glTranslatef((int) (pad), (int) (-params.height * 0.5f), 0f);
+            glTranslatef(Math.round(pad), Math.round(-pad), 0f);
             text.render(scale, loc, events);
         }
         glPopMatrix();
